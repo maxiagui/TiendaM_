4224fefd4d21cb645c64895636703db35ff2109c
AGUIRRE Maximiliano <maximiliano.aguirre@kantaribopemedia.com>
2020-09-18 09:51:55 -0300

    Initial commit

---
+++ app/src/main/java/com/example/testeo_primero/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.testeo_primero;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
